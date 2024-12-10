114e7352243cf6921d7afd67f0697baeac1beb30
chemist-god <godtenkorang72@gmail.com>
2024-12-10 01:23:00 +0000

    updated Bentogrid card

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -47,6 +47,9 @@ export const BentoGridItem = ({
         "row-span-1 relative rounded-3xl group/bento hover:shadow-xl transition duration-200 shadow-input dark:shadow-none p-4 dark:bg-black dark:border-white/[0.2] bg-white border border-transparent justify-between flex flex-col space-y-4",
         className
       )}
+      style={{
+        background: "linear-gradient(90deg, rgba(4,7,29,1) 0%, rgba(12,14,35,1) 100%)",
+      }}
     >
       <div className={cn(id === 6 && "flex justify-center", "h-full")}>
         {img && (
@@ -74,15 +77,18 @@ export const BentoGridItem = ({
             </div>
           </BackgroundGradientAnimation>
         )}
-      </div>
 
-      <div className="group-hover/bento:translate-x-2 transition duration-200">
-        <div className={`font-sans font-bold text-neutral-600 dark:text-neutral-200 mb-2 mt-2 ${titleClassName || ""}`}>
+        <div className={cn(
+            titleClassName, 'group-hover/benito:translate-x-2 transition duration-200 relative md:h-full min-h-40 flex flex-col px-5 p-5 lg:-10'
+        )}>
+            <div className="font-sans font-light text-[#c1c2d3] text-sm md:text-xs lg:text-base z-10">
+                {description}
+            </div>
+            <div className={`font-sans font-bold text-lg lg:text-3xl max-w-96 z-10  ${titleClassName || ""}`}>
           {title}
         </div>
-        <div className="font-sans font-normal text-neutral-600 text-xs dark:text-neutral-300">
-          {description}
         </div>
+
       </div>
     </div>
   );
